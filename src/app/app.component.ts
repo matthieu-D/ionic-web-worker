4fb48f3b9cbcacad7e9acfa70b2487896165a642
Matthieu Drula <matthieu.drula.hs@gmail.com>
2017-07-21 09:17:28 -0400

    all working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, public splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -19,4 +20,3 @@ export class MyApp {
     });
   }
 }
-
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -6,9 +6,17 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  result;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public ngZone: NgZone) {
+    const myWorker = new Worker('./assets/workers/worker.js');
 
+    myWorker.onmessage = (event) => {
+      this.ngZone.run(()=> {
+        this.result = event.data.join(',');
+      })
+    };
+
+    myWorker.postMessage('startQuickSort');
   }
-
 }
